db3d7213087b4745877396730d1f40985387a5b7
Cuongdevjs <nguyenmanhcuong.stf@gmail.com>
2020-08-29 09:20:15 +0700

    update: delete some folder unused

--- src/app/components/DrawerMenu/styled.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * styled DrawerMenu
- *
- */
-import styled from 'styled-components/macro';
-
-export const DrawerMenuWrapper = styled.div``;
--- src/app/components/Header/styled.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * styled Header
- *
- */
-import styled from 'styled-components/macro';
-
-export const HeaderWrapper = styled.div``;
